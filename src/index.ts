be1b1ce04bc16974386ebfe785933d6305dfada6
Tim <tim@rightly.co.uk>
2023-01-15 21:58:34 +0000

    Initial set up.

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import Cron from 'croner'
+import { Client } from 'disconnect'
+import 'dotenv/config'
+
+const roundPrice = (price: number) => Math.round(price * 100) / 100
+
+const Discogs = new Client({
+    userToken: process.env.DISCOGS_PAT
+}).marketplace()
+
+Cron('0 12 * * 7', async () => {
+    const { listings } = await Discogs.getInventory(process.env.DISCOGS_USER)
+
+    for (const listing of listings) {
+        const { id, price } = listing
+        const newPrice = roundPrice(price.value * 0.99)
+
+        await Discogs.editListing(id, { price: newPrice })
+    }
+})
